09be752ffd7a80e704d142a190cbe782df8bedd3
slledru <slledru@msn.com>
2018-05-24 11:19:37 -0600

    tools for secret key and hash

--- database/seeds_dev/001_insertUsers.js
+++ database/seeds_dev/001_insertUsers.js
@@ -4,7 +4,7 @@ exports.seed = (knex, Promise) => (
       knex('users').insert([{
         id: 1,
         username: 'hello@me.com',
-        password: '$2a$10$REbk3qsthnNR8XGTeir1J.jdVOva3aURv3Um4MVqa6L8C2G3eHpg2'
+        password: '$2a$10$X8oMGPVpcU33L/YGR7caTOIYnMLkt.Gv5WFWGom.q28V73Qlad6d2'
       }])
     ))
     .then(() => knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));`))
